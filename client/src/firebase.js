28c8ea35332f53f26fddc46183c564278d205f23
MANSI-MAHESHWARI <mansi.21b1541063@abes.ac.in>
2023-12-06 18:41:11 +0530

    admin

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+ // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASa4WpsujUKuQvnrC0d0b3z_NxmelQPRo",
+  authDomain: "itsneww-e5711.firebaseapp.com",
+  projectId: "itsneww-e5711",
+  storageBucket: "itsneww-e5711.appspot.com",
+  messagingSenderId: "1063438799821",
+  appId: "1:1063438799821:web:af8fbf8ca55fb7cc8eea84"
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
